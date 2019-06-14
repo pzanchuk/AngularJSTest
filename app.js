ad26edaab8143f34b2b397bfb2dcf72195c26f20
Pavel Zanchuk <buzzik@yahoo.com>
2019-06-13 23:56:14 -0700

    home stuff

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function(){
+myApp.controller('mainController', function($scope){
 
+  $scope.name = "Pavel"
+  $scope.last = "Zanchuk"
+  $scope.myFunction = function(){
+    return $scope.name + this.last;
+  }
+
+  console.log($scope);
+  
 });
+
+(function autoRun(){
+  alert("I am autorun")
+})();
+
+function changeBgBack(){
+  document.getElementById('change').style.backgroundColor = 'white'; 
+}
+
+function changeBg(color){
+  document.getElementById('change').style.backgroundColor = color; 
+  myVar = setTimeout(changeBgBack, 5000);
+  
+}
+
+var searchPeople = function(first, last){
+  return first + last;
+}
+
+var searchPeopleString = searchPeople.toString();
+
+console.log(typeof(searchPeople));
+console.log(typeof(searchPeopleString));
